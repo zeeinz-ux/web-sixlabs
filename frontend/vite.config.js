5c45a16f940589b6f2242d9a310de03d32032f88
zeeinz-ux <zeezeeinz@gmail.com>
2026-05-26 12:02:37 +0700

    chore: setting github actions deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/web-sixlabs/", // 👈 WAJIB TAMBAH BARIS INI (sesuai nama repository-mu)
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
